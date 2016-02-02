ba406a1caebf485aaca0c750d67457f2f8048b68
Daniel Jans <dev@dcja.de>
2016-02-02 21:39:45 +0100

    Added information about synchronization

--- src/de/dcja/prettygreatmusicplayer/PlaybackInfoHolder.java
+++ src/de/dcja/prettygreatmusicplayer/PlaybackInfoHolder.java
@@ -3,19 +3,25 @@ package de.dcja.prettygreatmusicplayer;
 /**
  * This is a singleton class to share playback information across the whole application from a
  * single spot rather than spamming broadcasts with all pieces of information.
+ *
+ * This singleton is going to be used by several threads simultaneously, therefore synchronization
+ * is required. When working with the playlist encapsulated in this class, synchronization should
+ * happen based on the <code>PlaybackInfoHolder</code> instance.
  */
 public class PlaybackInfoHolder {
     private static PlaybackInfoHolder instance;
 
     private Playlist activePlaylist;
     private MusicPlaybackService.PlaybackState playbackState;
+    private int trackDuration;
+    private int trackPosition;
 
     private PlaybackInfoHolder() {
         activePlaylist = null;
         playbackState = null;
     }
 
-    public static PlaybackInfoHolder getInstance() {
+    public static synchronized PlaybackInfoHolder getInstance() {
         if (instance == null) {
             instance = new PlaybackInfoHolder();
         }
@@ -23,19 +29,35 @@ public class PlaybackInfoHolder {
         return instance;
     }
 
-    public Playlist getActivePlaylist() {
+    public synchronized Playlist getActivePlaylist() {
         return activePlaylist;
     }
 
-    public void setActivePlaylist(Playlist activePlaylist) {
+    public synchronized void setActivePlaylist(Playlist activePlaylist) {
         this.activePlaylist = activePlaylist;
     }
 
-    public MusicPlaybackService.PlaybackState getPlaybackState() {
+    public synchronized MusicPlaybackService.PlaybackState getPlaybackState() {
         return playbackState;
     }
 
-    public void setPlaybackState(MusicPlaybackService.PlaybackState playbackState) {
+    public synchronized void setPlaybackState(MusicPlaybackService.PlaybackState playbackState) {
         this.playbackState = playbackState;
+    }
+
+    public synchronized int getTrackDuration() {
+        return trackDuration;
+    }
+
+    public synchronized void setTrackDuration(int trackDuration) {
+        this.trackDuration = trackDuration;
+    }
+
+    public synchronized int getTrackPosition() {
+        return trackPosition;
+    }
+
+    public synchronized void setTrackPosition(int trackPosition) {
+        this.trackPosition = trackPosition;
     }
 }
